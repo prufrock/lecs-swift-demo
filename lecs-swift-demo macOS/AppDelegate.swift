377a1213f97a5fc603eb4d7fc21214ac8d6a001b
David Kanenwisher <d.kanen@gmail.com>
2023-07-22 13:24:07 -0500

    Add multi-platform starter

---
+++ lecs-swift-demo macOS/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  lecs-swift-demo macOS
+//
+//  Created by David Kanenwisher on 7/22/23.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
