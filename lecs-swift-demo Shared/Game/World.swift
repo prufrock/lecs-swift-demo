6bb4c9190c815ffddec826143c15d08e065a09f6
David Kanenwisher <d.kanen@gmail.com>
2023-07-24 20:45:47 -0500

    Add an array based system for comparison

--- lecs-swift-demo Shared/Game/World.swift
+++ lecs-swift-demo Shared/Game/World.swift
@@ -26,6 +26,11 @@ struct World {
     }
 
     var updatePositionSystem: LECSSystemId
+    var updateColorSystem: LECSSystemId
+
+    var entities: [[LECSComponent]]
+
+    var entitiesUpdatePositionSystem: ([LECSComponent]) -> [LECSComponent]
 
     public var playing = false
 
@@ -40,14 +45,22 @@ struct World {
         self.config = config
         self.map = map
         self.entityManager = ECSBigObjectEntityManager()
+        self.entities = [
+            Array<LECSId>(repeating: LECSId(id: 0), count: 300),
+            Array<LECSName>(repeating: LECSName(name: ""), count: 300),
+            Array<LECSPosition2d>(repeating: LECSPosition2d(x: 0, y: 0), count: 300),
+            Array<LECSVelocity2d>(repeating: LECSVelocity2d(x: 0, y: 0), count: 300)
+        ]
 
         ecs = LECSWorldFixedSize(archetypeSize: 400)
-        for i in 0..<40 {
+        for i in 0..<25 {
             let b = try! ecs.createEntity("b\(i)")
             let column = Float(Int(i % 10))
             let row = Float(Int(i / 10))
             try! ecs.addComponent(b, LECSPosition2d(x: column, y: row))
             try! ecs.addComponent(b, LECSVelocity2d(x: 0, y: 0.01))
+            let color = ColorA(Color.red)
+            try! ecs.addComponent(b, EntityColor(color: color))
         }
         let bunny = try! ecs.createEntity("bunny")
         try! ecs.addComponent(bunny, LECSPosition2d(x: 3, y: 4))
@@ -55,6 +68,19 @@ struct World {
         let fox = try! ecs.createEntity("fox")
         try! ecs.addComponent(fox, LECSPosition2d(x: 2, y: 7))
         try! ecs.addComponent(fox, LECSVelocity2d(x: 0, y: 0.01))
+
+        for i in 0..<25 {
+            let column = Float(Int(i % 10))
+            let row = Float(Int(i / 10))
+            let id = LECSId(id: UInt(i))
+            let name = LECSName(name: "b\(i)")
+            let position = LECSPosition2d(x: column, y: row)
+            let velocity = LECSVelocity2d(x: 0, y: 0.01)
+            entities[0][i] = id
+            entities[1][i] = name
+            entities[2][i] = position
+            entities[3][i] = velocity
+        }
 
         updatePositionSystem = ecs.addSystem(
             "UpdatePosition",
@@ -70,6 +96,42 @@ struct World {
                 }
 
                 return [position, velocity]
+        }
+
+
+        updateColorSystem = ecs.addSystem(
+            "UpdateColor",
+            selector: [EntityColor.self]) { world, components in
+                var color = components[0] as! EntityColor
+
+                if color.increasing {
+                    color.g = color.g + 0.001
+                } else {
+                    color.g = color.g - 0.001
+                }
+
+
+                if (color.g <= 0.1) {
+                    color.increasing = true
+                } else if (color.g >= 1) {
+                    color.increasing = false
+                }
+
+                return [color]
+        }
+
+        entitiesUpdatePositionSystem = { components in
+            var position = components[0] as! LECSPosition2d
+            var velocity = components[1] as! LECSVelocity2d
+
+            position.y = position.y + velocity.velocity.y
+            if position.y > 10 {
+                velocity.velocity = Float2(velocity.velocity.x, -1 * velocity.velocity.y)
+            } else if position.y < 0 {
+                velocity.velocity = Float2(velocity.velocity.x, -1 * velocity.velocity.y)
+            }
+
+            return [position, velocity]
         }
         reset()
     }
@@ -128,21 +190,22 @@ struct World {
     mutating func update(timeStep: Float, input: Input) {
         var gameInput = GameInput(externalInput: input, selectedButtonId: nil)
 
-        if (input.isTouched) {
-            let position = input.touchCoordinates
-                    .screenToNdc(screenWidth: input.viewWidth, screenHeight: input.viewHeight, flipY: true)
-                    .ndcToWorld(camera: hudCamera!.camera!)
-            let eLocation = entityManager.createProp(id: "touchLocation", position: position, radius: 0.12, camera: .hud)
-
-            if let collision = eLocation.collision {
-                if let selected = entityManager.pickCollision(at: collision) {
-                    print("collided entity \(selected.id)")
-                    gameInput.selectedButton = selected
-                }
-            }
-        }
+//        if (input.isTouched) {
+//            let position = input.touchCoordinates
+//                    .screenToNdc(screenWidth: input.viewWidth, screenHeight: input.viewHeight, flipY: true)
+//                    .ndcToWorld(camera: hudCamera!.camera!)
+//            let eLocation = entityManager.createProp(id: "touchLocation", position: position, radius: 0.12, camera: .hud)
+//
+//            if let collision = eLocation.collision {
+//                if let selected = entityManager.pickCollision(at: collision) {
+//                    print("collided entity \(selected.id)")
+//                    gameInput.selectedButton = selected
+//                }
+//            }
+//        }
 
         ecs.process(system: updatePositionSystem)
+        ecs.process(system: updateColorSystem)
 
         // Update all of the entities
         entityManager.entities.forEach { entity in
@@ -151,6 +214,12 @@ struct World {
                 entityManager.update(newEntity.update(input: gameInput, world: &self))
             }
         }
+
+//        for i in 0..<(entities[0].count - 1) {
+//            let updated = entitiesUpdatePositionSystem([entities[2][i], entities[3][i]])
+//            entities[2][i] = updated[0]
+//            entities[3][i] = updated[1]
+//        }
 
         if let camera = hudCamera {
             hudCamera = entityManager.find(camera.id)
@@ -170,5 +239,39 @@ struct World {
         } else {
             camera = overHeadCamera
         }
+    }
+}
+
+struct EntityColor: LECSComponent {
+    var r: Float
+    var g: Float
+    var b: Float
+    var a: Float
+    var increasing: Bool
+
+    public init(color: ColorA, increasing: Bool = false) {
+        r = color.r
+        g = color.g
+        b = color.b
+        a = color.a
+        self.increasing = increasing
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.unkeyedContainer()
+        try container.encode(self.r)
+        try container.encode(self.g)
+        try container.encode(self.b)
+        try container.encode(self.a)
+        try container.encode(self.increasing)
+    }
+
+    public init(from decoder: Decoder) throws {
+        var container = try decoder.unkeyedContainer()
+        r = try container.decode(Float.self)
+        g = try container.decode(Float.self)
+        b = try container.decode(Float.self)
+        a = try container.decode(Float.self)
+        increasing = try container.decode(Bool.self)
     }
 }
--- lecs-swift-demo Shared/Render/RNDRRenderer.swift
+++ lecs-swift-demo Shared/Render/RNDRRenderer.swift
@@ -92,6 +92,7 @@ class RNDRMetalRenderer: RNDRRenderer {
 
 //        renderSceneGraph(game.world.scene, game: game, screen: screen, encoder: encoder)
         render(game: game, screen: screen, encoder: encoder)
+//        render(game: game, entities: game.world.entities, screen: screen, encoder: encoder)
         encoder.endEncoding()
 
         guard let drawable = view.currentDrawable else {
@@ -149,8 +150,53 @@ class RNDRMetalRenderer: RNDRRenderer {
 
     private func render(game: Game, screen: ScreenDimensions, encoder: MTLRenderCommandEncoder) {
         let camera: ECSGraphics.Camera = .hud
-        game.world.ecs.select([LECSPosition2d.self]) { world, components in
+        game.world.ecs.select([LECSPosition2d.self, EntityColor.self]) { world, components in
             let point = components[0] as! LECSPosition2d
+            let color = components[1] as! EntityColor
+
+            let model: Model = Square()
+
+            let viewToClip = Float4x4.identity()
+            let clipToNdc = Float4x4.identity()
+            let ndcToScreen = Float4x4.identity()
+
+            //TODO: This is a hack, need to find a better way to get the camera. Maybe in the scene the graphic is a child of?
+            var finalTransform: Float4x4
+            switch camera {
+            case .hud:
+                finalTransform = ndcToScreen
+                    * clipToNdc
+                    * viewToClip
+                    * game.world.hudCamera!.camera!.projection()
+                    * Float4x4.translate(Float2(point.x, point.y))
+                    * Float4x4.scale(x: 0.1, y: 0.1, z: 0.1)
+            case .world:
+                finalTransform = ndcToScreen
+                    * clipToNdc
+                    * viewToClip
+                    * game.world.camera!.camera!.projection()
+                    * Float4x4.translate(Float2(point.x, point.y))
+            }
+
+            let buffer = device.makeBuffer(bytes: model.v, length: MemoryLayout<Float3>.stride * model.v.count, options: [])
+
+            encoder.setRenderPipelineState(vertexPipeline)
+            encoder.setDepthStencilState(depthStencilState)
+            encoder.setVertexBuffer(buffer, offset: 0, index: 0)
+            encoder.setVertexBytes(&finalTransform, length: MemoryLayout<Float4x4>.stride, index: 1)
+
+            var fragmentColor = Float4(x: color.r, y: color.g, z: color.b, w: color.a)
+
+            encoder.setFragmentBuffer(buffer, offset: 0, index: 0)
+            encoder.setFragmentBytes(&fragmentColor, length: MemoryLayout<Float3>.stride, index: 0)
+            encoder.drawPrimitives(type: model.primitiveType, vertexStart: 0, vertexCount: model.v.count)
+        }
+    }
+
+    private func render(game: Game, entities: [[LECSComponent]], screen: ScreenDimensions, encoder: MTLRenderCommandEncoder) {
+        let camera: ECSGraphics.Camera = .hud
+        for i in 0..<(entities[0].count - 1) {
+            let point = entities[2][i] as! LECSPosition2d
 
             let model: Model = Square()
 
